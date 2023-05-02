05901e5298159f64381c88d14425687abd5bdc13
Artyom <itcoding76@gmail.com>
2023-05-02 21:23:49 +0200

    Home Work 05 fixed file`s path

--- oop_java/homeworks/HW05/src/personal/Main.java
+++ oop_java/homeworks/HW05/src/personal/Main.java
@@ -6,8 +6,11 @@ import personal.views.ViewUser;
 
 public class Main {
     public static void main(String[] args) {
+        String pathProject = System.getProperty("user.dir");
+        String pathFile = pathProject.concat("/oop_java/homeworks/HW05/src/personal/users.md");
+        System.out.println(pathFile);
 //        FileOperation fileOperation = new FileOperationImpl("users.txt");
-        FileOperation fileOperation = new FileOperationImplMd("users.md");
+        FileOperation fileOperation = new FileOperationImplMd(pathFile);
         Repository repository = new RepositoryFile(fileOperation);
         UserController controller = new UserController(repository);
         ViewUser view = new ViewUser(controller);
