ddf7cc7e9d1e5e7e9f23309343107daec4b011de
Artyom <itcoding76@gmail.com>
2023-05-06 17:36:44 +0200

    Initial commit for task 2

--- ru.geekbrains.javaAPI/homeWorksAPI/homeWorkAPI_02/src/Main.java
+++ ru.geekbrains.javaAPI/homeWorksAPI/homeWorkAPI_02/src/Main.java
@@ -2,19 +2,21 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String path = System.getProperty("user.dir");
-        String pathFileToRead = path.concat("\\ru.geekbrains.javaAPI\\homeWorksAPI\\homeWorkAPI_02\\src\\data.json");
-        String pathFileToSave = path.concat("\\ru.geekbrains.javaAPI\\homeWorksAPI\\homeWorkAPI_02\\src\\data.txt");
-
-        String[] data = Task1.readText(pathFileToRead);
-        for(String line: data){
-            System.out.println(line);
-        }
-        StringBuilder sb = Task1.parseText(data[0]);
-        System.out.println();
-        System.out.println(sb);
-        System.out.println("Запись в файл:");
-        Task1.saveText(sb.toString(), pathFileToSave);
+        //region Решение первой задачи
+//        String path = System.getProperty("user.dir");
+//        String pathFileToRead = path.concat("\\ru.geekbrains.javaAPI\\homeWorksAPI\\homeWorkAPI_02\\src\\data.json");
+//        String pathFileToSave = path.concat("\\ru.geekbrains.javaAPI\\homeWorksAPI\\homeWorkAPI_02\\src\\data.txt");
+//
+//        String[] data = Task1.readText(pathFileToRead);
+//        for(String line: data){
+//            System.out.println(line);
+//        }
+//        StringBuilder sb = Task1.parseText(data[0]);
+//        System.out.println();
+//        System.out.println(sb);
+//        System.out.println("Запись в файл:");
+//        Task1.saveText(sb.toString(), pathFileToSave);
+        //endregion
 
     }
 }
