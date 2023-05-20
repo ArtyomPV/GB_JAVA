146112eba8ba70288f2f50b98aaa3afca6424362
Artyom <itcoding76@gmail.com>
2023-05-20 20:09:51 +0200

    Seminar 06 Single responsible principle 01 fixed according to principle

--- oop_java/seminars/seminar06/Solid1Srp1/src/Main.java
+++ oop_java/seminars/seminar06/Solid1Srp1/src/Main.java
@@ -1,10 +1,15 @@
+import srp.Draw;
 import srp.Point;
 import srp.Square;
+
+import javax.swing.*;
 
 public class Main {
     public static void main(String[] args) {
         Square square = new Square(new Point(1, 1), 5);
         System.out.printf("Площадь фигуры: %d \n", square.getArea());
-        square.draw();
+        Draw draw = new Draw(10);
+
+        draw.draw(5*2);
     }
 }
---
+++ oop_java/seminars/seminar06/Solid1Srp1/src/srp/Draw.java
@@ -0,0 +1,27 @@
+package srp;
+
+public class Draw {
+    private int side;
+
+    public Draw(int side) {
+        this.side = side;
+    }
+
+    public  void draw(int side){
+        for (int i = 0; i < side; i++) {
+            System.out.print("* ");
+        }
+        System.out.println();
+        for (int i = 0; i < side - 2; i++) {
+            System.out.print("* ");
+            for (int j = 1; j < side - 1; j++) {
+                System.out.print("  ");
+            }
+            System.out.println("*");
+
+        }
+        for (int i = 0; i < side; i++) {
+            System.out.print("* ");
+        }
+    }
+}
