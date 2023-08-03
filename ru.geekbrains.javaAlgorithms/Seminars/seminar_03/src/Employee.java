04a54ffc28feba35b40d55dda5851ffd4aec89b1
Artyom <itcoding76@gmail.com>
2023-08-03 08:48:38 +0200

    created class Employee

---
+++ ru.geekbrains.javaAlgorithms/Seminars/seminar_03/src/Employee.java
@@ -0,0 +1,28 @@
+public class Employee {
+    private String name;
+    private int age;
+
+    public String getName() {return name;}
+
+    public void setName(String name) {this.name = name;}
+
+    public int getAge() {return age;}
+
+    public void setAge(int age) {this.age = age;}
+
+    @Override
+    public boolean equals(Object obj) {
+        if(this == obj){
+            return true;
+        }
+        if(obj instanceof Employee employee){
+            return employee.name.equals(this.name) && employee.age == this.age;
+        }
+        return false;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("[%s - %d]", this.name, this.age);
+    }
+}
